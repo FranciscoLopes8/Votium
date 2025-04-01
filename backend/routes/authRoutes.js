b17ea340547bdde8e9b5938cf28aacd79214fe11
FRANCISCO DOMINGUES LOPES <al78973@alunos.utad.pt>
2025-04-01 12:41:15 +0100

    codigo pessoal

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,20 +1,25 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const User = require("../models/User");
+const crypto = require('crypto');
 
 const router = express.Router();
 
-// Rota para registrar usuário
+const generateSecureCode = (length = 8) => {
+    return crypto.randomBytes(length).toString('hex').slice(0, length);
+};
+
 router.post("/register", async (req, res) => {
-    const { primeiroNome, ultimoNome, telefone, senha, role = "User" } = req.body;
+    const { primeiroNome, ultimoNome, telefone, senha, role = "User", codigoPessoal } = req.body;
 
     try {
         const userExistente = await User.findOne({ telefone });
-        if (userExistente) return res.status(400).json({ message: "Telefone já cadastrado" });
+        if (userExistente) return res.status(400).json({ message: "Telefone já registado" });
 
         const senhaHash = await bcrypt.hash(senha, 10);
+        codigoPessoal = generateSecureCode();
 
-        const novoUsuario = new User({ primeiroNome, ultimoNome, telefone, senha: senhaHash, role: role || "user" });
+        const novoUsuario = new User({ primeiroNome, ultimoNome, telefone, senha: senhaHash, role: role || "user", codigoPessoal });
         await novoUsuario.save();
 
         res.status(201).json({ message: "Conta Criada" });
